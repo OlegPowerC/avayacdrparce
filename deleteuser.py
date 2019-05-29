99808826e64fb1f79fd85c26d48843a259b9a1f9
OlegPowerC <oleg@powerc.ru>
2019-05-29 17:43:45 +0300

    Added condition codes

---
+++ deleteuser.py
@@ -0,0 +1,16 @@
+#!/opt/pyenvs/1/bin/python3
+import keyring
+import argparse
+
+if __name__ == '__main__':
+    parcer = argparse.ArgumentParser(description="Set passord in Keyring")
+    parcer.add_argument('-u', type=str, help="User name", required=True)
+    parcer.add_argument('-k', type=str, help="KeyChainName", required=True)
+    args = parcer.parse_args()
+    if len(args.u) < 4:
+        print("Too short username")
+        exit(1)
+    if len(args.k) < 4:
+        print("Too short keychainname")
+        exit(1)
+    keyring.delete_password(args.k,args.u)
